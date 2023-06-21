25dbd5cfc3cbbbefc9076d1f6b537f96eda68cb6
Afrikind <anthony.madukaku@acity.edu.gh>
2023-06-21 05:02:18 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,69 +1,9 @@
-#!/usr/bin/python3
-"""test for state"""
-import unittest
-import os
-from models.state import State
-from models.base_model import BaseModel
-import pep8
-
-
-class TestState(unittest.TestCase):
-    """this will test the State class"""
-
-    @classmethod
-    def setUpClass(cls):
-        """set up for test"""
-        cls.state = State()
-        cls.state.name = "CA"
-
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.state
-
-    def tearDown(self):
-        """teardown"""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
-    def test_pep8_Review(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/state.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
-
-    def test_checking_for_docstring_State(self):
-        """checking for docstrings"""
-        self.assertIsNotNone(State.__doc__)
-
-    def test_attributes_State(self):
-        """chekcing if State have attributes"""
-        self.assertTrue('id' in self.state.__dict__)
-        self.assertTrue('created_at' in self.state.__dict__)
-        self.assertTrue('updated_at' in self.state.__dict__)
-        self.assertTrue('name' in self.state.__dict__)
-
-    def test_is_subclass_State(self):
-        """test if State is subclass of BaseModel"""
-        self.assertTrue(issubclass(self.state.__class__, BaseModel), True)
-
-    def test_attribute_types_State(self):
-        """test attribute type for State"""
-        self.assertEqual(type(self.state.name), str)
-
-    @unittest.skipIf(os.environ['HBNB_TYPE_STORAGE'] == 'db',
-                     'Invalid storage mode')
-    def test_save_State(self):
-        """test if the save works"""
-        self.state.save()
-        self.assertNotEqual(self.state.created_at, self.state.updated_at)
-
-    def test_to_dict_State(self):
-        """test if dictionary works"""
-        self.assertEqual('to_dict' in dir(self.state), True)
-
-
-if __name__ == "__main__":
-    unittest.main()
+echo 'SELECT * FROM reviews\G' | mysql -uhbnb_dev -p hbnb_dev_db
+Enter password: 
+*************************** 1. row ***************************
+        id: f2616ff2-f723-4d67-85dc-f050a38e0f2f
+created_at: 2017-11-10 04:06:25
+updated_at: 2017-11-10 04:06:25
+      text: Amazing place, huge kitchen
+  place_id: ed72aa02-3286-4891-acbc-9d9fc80a1103
+   user_id: d93638d9-8233-4124-8f4e-17786592908b
