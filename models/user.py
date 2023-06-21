e321d4ba99221e31dd617038633ce511b944dc83
Afrikind <anthony.madukaku@acity.edu.gh>
2023-06-21 04:59:16 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,11 +1,38 @@
-#!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
-
-
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class attribute __tablename__
+represents the table name, places
+class attribute city_id
+represents a column containing a string (60 characters)
+can’t be null
+is a foreign key to cities.id
+class attribute user_id
+represents a column containing a string (60 characters)
+can’t be null
+is a foreign key to users.id
+class attribute name
+represents a column containing a string (128 characters)
+can’t be null
+class attribute description
+represents a column containing a string (1024 characters)
+can be null
+class attribute number_rooms
+represents a column containing an integer
+can’t be null
+default value: 0
+class attribute number_bathrooms
+represents a column containing an integer
+can’t be null
+default value: 0
+class attribute max_guest
+represents a column containing an integer
+can’t be null
+default value: 0
+class attribute price_by_night
+represents a column containing an integer
+can’t be null
+default value: 0
+class attribute latitude
+represents a column containing a float
+can be null
+class attribute longitude
+represents a column containing a float
+can be null
